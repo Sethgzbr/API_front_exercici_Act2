4637e00c78dc7a481203da53be8146d86cc77444
Seth <sethgzbr@gmail.com>
2024-10-16 16:25:02 +0200

    alumne_schema modificat

--- API/alumne.py
+++ API/alumne.py
@@ -1,13 +1,11 @@
 def estudiant_schema(student) -> dict:
-    return {"IdAlumne": student[0],
-            "IdAula": student[1],
-            "nomAlumne": student[2],
-            "cicle": student[3],
-            "curs": student[4],
-            "grup": student[5],
-            "createdAt": student[6],
-            "updatedAt": student[7]
+     return {"NomAlumne": fetchAlumnes[0],
+            "Cicle": fetchAlumnes[1],
+            "Curs": fetchAlumnes[2],
+            "Grup": fetchAlumnes[3],
+            "DescAula": fetchAlumnes[4]
             }
+
 
 def alumne_schema(alumne) -> dict:
     return [estudiant_schema(estudiant) for estudiant in alumne] 
